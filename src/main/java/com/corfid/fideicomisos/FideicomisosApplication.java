12aa5f6d756ac63a77881a83a463debfa3ff803d
User <User@192.168.1.3>
2020-09-13 15:41:50 -0500

    proyecto inicial

---
+++ src/main/java/com/corfid/fideicomisos/FideicomisosApplication.java
@@ -0,0 +1,13 @@
+package com.corfid.fideicomisos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FideicomisosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FideicomisosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corfid/fideicomisos/FideicomisosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corfid.fideicomisos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FideicomisosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
